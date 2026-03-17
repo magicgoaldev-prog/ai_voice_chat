70c929624b4b5c51e7bada03d3e689f3cf6e64ce
magicgoal <magicgoal.dev@gmail.com>
2026-03-17 17:11:34 +1000

    integrate posthog

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import posthog from 'posthog-js'
+import { PostHogProvider } from '@posthog/react'
 import App from './App.tsx'
 import './index.css'
 
+const posthogKey = import.meta.env.VITE_POSTHOG_KEY
+const posthogHost = import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com'
+if (posthogKey) {
+  posthog.init(posthogKey, { api_host: posthogHost })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 )
